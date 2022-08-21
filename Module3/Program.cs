63a6e90c1578c2a4fe971c070fd3e78b73804c78
Mellomanprost <anoppa@mail.ru>
2022-08-21 13:12:29 +0300

    Добавьте файлы проекта.

---
+++ Module3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyName = "Jane";
+
+            Console.WriteLine(MyName);
+
+            Console.WriteLine("Привет, мир");
+            Console.WriteLine("Мне 27 лет");
+            Console.WriteLine("My name is Jane");
+
+            Console.ReadKey();
+        }
+    }
+}
