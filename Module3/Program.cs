5e7adb3d2cb31d434431349ba72f5ae6495b9084
Mellomanprost <anoppa@mail.ru>
2022-08-21 13:16:03 +0300

    Добавлены управляюзщие символы

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -14,9 +14,9 @@ namespace Module3
 
             Console.WriteLine(MyName);
 
-            Console.WriteLine("Привет, мир");
-            Console.WriteLine("Мне 27 лет");
-            Console.WriteLine("My name is Jane");
+            Console.WriteLine("\tПривет, мир");
+            Console.WriteLine("\tМне 27 лет");
+            Console.WriteLine("\tMy name is\n  Jane");
 
             Console.ReadKey();
         }
