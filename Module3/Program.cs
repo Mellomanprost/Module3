b108ca9d86d487894cc07b1dddf6978e2a7708c2
Mellomanprost <anoppa@mail.ru>
2022-08-22 20:39:05 +0300

    Added exercise 3.7.4

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -15,7 +15,7 @@ namespace Module3
             bool HaveIPet = true;
             double MyShoeSize = 37.5;
 
-            Console.WriteLine("Hello, world! This information from exercise.");
+            Console.WriteLine("Hello, world! This information from exercise 3.2.");
             Console.WriteLine("My name is " + MyName);
             Console.WriteLine("My age is " + MyAge);
             Console.WriteLine("Do I have a pet? " + HaveIPet);
@@ -26,6 +26,16 @@ namespace Module3
             Console.WriteLine("My age is 31");
             Console.WriteLine("I don't have a pet");
             Console.WriteLine("My shoe size is 42");
+
+            Console.WriteLine("\nEXERCISE 3.7.");
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            byte age = checked ((byte) int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.WriteLine("What is your favorite day of week?");
+            DayOfWeek favDay = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorite day is " + favDay);
 
             Console.ReadKey();
         }
